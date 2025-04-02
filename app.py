351522898c261e13c0dd8278f942004b79294fb9
ankitamohanty1509 <148987768+ankitamohanty1509@users.noreply.github.com>
2025-04-02 15:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/products', methods=['GET'])
-
 def get_products():
-
-products = [
-
-{"id": 1, "name": "Laptop", "price": 800},
-
-{"id": 2, "name": "Smartphone", "price": 600},
-
-]
-
-return jsonify(products)
+    products = [
+        {"id": 1, "name": "Laptop", "price": 800},
+        {"id": 2, "name": "Smartphone", "price": 600},
+    ]
+    return jsonify(products)
 
 if __name__ == "__main__":
-
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
